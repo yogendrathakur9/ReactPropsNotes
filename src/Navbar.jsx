8dbd99d3c71265dd3f162e5d5031da8cc2b124e4
yogendrathakur <yogenthakur9@gmail.com>
2023-02-03 11:21:30 +0530

    massage

---
+++ src/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from 'prop-types';
+function Navbar({title , link}){
+    return(
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">{title}</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">{link}</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+
+}
+
+
+Navbar.defaultProps = {
+    link: 'home',
+    title: 'navbar'
+  };
+  Navbar.propTypes = {
+    title: PropTypes.string,
+    linkL:PropTypes.string
+  };
+
+
+export default Navbar
+
